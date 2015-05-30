e593540991e2c22d9c3b10a2aba75ec096ed6d9e
vitnam <vartemjev@outlook.com>
2015-05-30 08:17:20 +0300

    refactor

---
+++ lngpok/QuickSort.java
@@ -0,0 +1,14 @@
+package ads_001.lngpok;
+
+/**
+ * Created by Victor Artemjev on 19.05.2015.
+ */
+public class QuickSort {
+public static void sort(int[] data) {
+    quickSortRecursive(data, 0, 0);
+}
+
+    private static void quickSortRecursive(int[] data, int left, int right) {
+
+    }
+}
